5dcd77258c53dbcdca1f60d07ffe3a34c7b8eac3
Yuancheng Zhang <old.far.hot@gmail.com>
2017-07-31 17:11:35 -0500

    update

--- js/custom.js
+++ js/custom.js
@@ -4,4 +4,25 @@ $().ready(function () {
     var month = today.getMonth() + 1;
     var dates = today.getDate();
     $(".cy-today").html(year + "年" + month+"月"+dates+"日");
+    $(".cy-dept-doctor-list").hide();
+
+    $("#cy-dept-select").change(function () {
+        var value = $("#cy-dept-select option:selected").val();
+        var dept = "";
+        switch(value) {
+            case "dept-pediatrics":
+                dept = "cy-pediatrics";
+                break;
+            case "dept-gynaecology":
+                dept = "cy-gynaecology";
+                break;
+            default:
+                dept = '';
+        }
+
+        if (dept != '') {
+            $(".cy-dept-doctor-list").hide();
+            $("#"+dept).show();
+        }
+    })
 });
